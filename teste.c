1ddca6203caf9a0a6a6f329606f88e6064139bc0
Laura0silva <125465992+Laura0silva@users.noreply.github.com>
2025-04-07 14:49:07 +0000

    Desafio novato finalizado

--- teste.c
+++ teste.c
@@ -18,7 +18,7 @@ int main() {
     int navio2V[2];     // vetor para as coordenadas do navio 2
 
     printf("    Tabuleiro: Batalha Naval\n");
-    printf("   1  2  3  4  5  6  7  8  9  10\n");
+    printf("   1  2  3  4  5  6  7  8  8  10\n");
 
     for (int i = 0; i < 10; i++) {            // loop para percorrer as linhas da matriz
         printf("%d  ", i + 1);                // imprime a coordenada na linha
@@ -35,12 +35,12 @@ int main() {
     printf("Agora da coluna onde o navio vai começar (de 1 a 8), exemplo: digite 2 e vai ocupar os espaços 2, 3 e 4: ");
     scanf("%d", &navio1H[1]);   // armazena a coordenada no índice 1 do vetor
 
-    if (navio1H[0] < 11 && navio1H[1] < 9) {    // verifica se as coordenadas são válidas
-        for (int j = 0; j < 3; j++) {           // loop para repetir 3 vezes
+    if (navio1H[0] < 11 && navio1H[1] < 9) {  // verifica se as coordenadas são válidas
+        for (int j = 0; j < 3; j++) {         // loop para repetir 3 vezes
             tabuleiro[navio1H[0] - 1][navio1H[1] - 1] = 3;  // define navio
-            navio1H[1]++;                       // avança para o próximo espaço
+            navio1H[1]++;                    // avança para o próximo espaço
         }
-    } else {                                    // caso a coordenada dada for inválida
+    } else {   // caso a coordenada dada for inválida
         printf("Você escolheu um valor inválido!!!\n");
     }
 
@@ -51,32 +51,18 @@ int main() {
     printf("Agora da coluna (de 1 a 10): ");
     scanf("%d", &navio2V[1]);   // armazena a coordenada no índice 1 do vetor
 
-    if (navio2V[1] < 11 && navio2V[0] < 9) {    // verifica se as coordenadas são válidas
-        int sobreposicao = 0;
-
-        // Verifica se algum dos 3 espaços está ocupado
-        for (int i = 0; i < 3; i++) {           // loop para repetir 3 vezes
-            if (tabuleiro[navio2V[0] - 1 + i][navio2V[1] - 1] == 3) {
-                sobreposicao = 1;
-                break;
-            }
+    if (navio2V[1] < 11 && navio2V[0] < 9) {  // verifica se as coordenadas são válidas
+        for (int i = 0; i < 3; i++) {         // loop para repetir 3 vezes
+            tabuleiro[navio2V[0] - 1][navio2V[1] - 1] = 3;  // define navio
+            navio2V[0]++;                    // avança para o próximo espaço
         }
-        if (sobreposicao) {         // Se for verdadeiro, não executa o comando.
-            printf("\nOs navios não podem se sobrepor!\n");
-        } 
-        else {                      // Posiciona o navio
-            for (int i = 0; i < 3; i++) {
-                tabuleiro[navio2V[0] - 1][navio2V[1] - 1] = 3;
-                navio2V[0]++;       // avança para o próximo espaço
-            }
-        }
-    } else {
+    } else {   // caso a coordenada dada for inválida
         printf("Você escolheu um valor inválido!!!\n");
     }
 
     // Imprime o tabuleiro novamente
-    printf("\n    Tabuleiro: Batalha Naval\n");
-    printf("   1  2  3  4  5  6  7  8  9  10\n");
+    printf("    Tabuleiro: Batalha Naval\n");
+    printf("   1  2  3  4  5  6  7  8  8  10\n");
 
     for (int i = 0; i < 10; i++) {
         printf("%d  ", i + 1);
